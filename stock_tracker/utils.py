7d79be33c7bae67fa387ff0212401c49deeaa2d6
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-04-28 18:00:08 +0800

    refactor: setup logging now has the correct default logging path value

--- stock_tracker/utils.py
+++ stock_tracker/utils.py
@@ -1,8 +1,10 @@
 import logging.config
 import yaml
 
+from config import DEFAULT_LOGGING_CONFIG_PATH
 
-def setup_logging(config_path: str = "logging_config.yaml") -> None:
+
+def setup_logging(config_path: str = DEFAULT_LOGGING_CONFIG_PATH) -> None:
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
         logging.config.dictConfig(config)
