a3f8730bb108ac9cf7bb36b056b6ec0b968528dd
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-05-08 20:27:34 +0800

    test: removed test_db() fixture from test_DB.py, functionality is now in conftest.py

--- tests/test_DB.py
+++ tests/test_DB.py
@@ -3,10 +3,3 @@ from pytest_mock import MockerFixture
 
 from stock_tracker.config import AppConfig
 from stock_tracker.db import Database
-
-@pytest.fixture
-def test_db():
-    db: Database = Database(DEFAULT_DB_NAME)
-    with db:
-        db.create_tables_if_not_exists()
-        yield db
