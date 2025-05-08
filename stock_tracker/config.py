64fd53e2b05a89daaa94a365f84a368a0808b503
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-05-08 20:13:58 +0800

    chore: remove un-used dataclasses.replace import

--- stock_tracker/config.py
+++ stock_tracker/config.py
@@ -3,7 +3,7 @@ import logging
 import os
 import yaml
 import argparse
-from dataclasses import dataclass, fields, MISSING, replace
+from dataclasses import dataclass, fields, MISSING
 from pathlib import Path
 from typing import (
     Any,
