f3eb3f77a35536664bd3f539e49379406e22a1f9
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-05-09 13:39:01 +0800

    refactor: move get_stock_price() from main.py to yfinanc_api.py

--- stock_tracker/main.py
+++ stock_tracker/main.py
@@ -43,29 +43,6 @@ def write_json_to_file(data: dict[Any, Any], filename: str) -> None:
         print(f"Error writing to file '{filename}': {e}")
 
 
-def get_stock_price(
-    ticker: str, session: CachedLimiterSession, exchange: str | None = None
-) -> float:
-    """
-    Retrieves the latest stock price for a given symbol using the Alpha Vantage API.
-    Returns a tuple: (price: float or None, error: str or None).
-    """
-    # TODO: Specify the currency, and convert it if necessary
-    # TODO: Ensure can hangle stocks with the same ticker, that are listed in different exchanges
-    try:
-        logger.debug(f"Checking {ticker} price in exchange: {exchange}.")
-        full_ticker: str = f"{ticker}.{exchange}" if exchange else ticker
-        data: yf.Ticker = yf.Ticker(full_ticker, session=session)
-
-        last_price = data.fast_info.last_price
-        if last_price is not None:
-            logger.debug(f"{ticker} price is: {last_price}.")
-            return float(last_price)
-        raise ValueError(f"No price found for {full_ticker}")
-    except Exception as e:
-        raise ValueError(f"Failed to retrieve price for {ticker} at {exchange}: {e}")
-
-
 def calculate_order_capital_gains(order: dict[str, str], current_price: float) -> float:
     # Basic gain calculation
     # Diff between order price and current price
--- stock_tracker/yfinance_api.py
+++ stock_tracker/yfinance_api.py
@@ -1,3 +1,4 @@
+import yfinance as yf
 from pyrate_limiter import Duration, Limiter, RequestRate
 from requests import Session
 from requests_cache import CacheMixin, SQLiteCache
@@ -33,3 +34,26 @@ def get_yfinance_session(
         bucket_class=MemoryQueueBucket,
         backend=SQLiteCache(db_path=cache_path),
     )
+
+
+def get_stock_price(
+    ticker: str, session: CachedLimiterSession, exchange: str | None = None
+) -> float:
+    """
+    Retrieves the latest stock price for a given symbol using the Alpha Vantage API.
+    Returns a tuple: (price: float or None, error: str or None).
+    """
+    # TODO: Specify the currency, and convert it if necessary
+    # TODO: Ensure can hangle stocks with the same ticker, that are listed in different exchanges
+    try:
+        logger.debug(f"Checking {ticker} price in exchange: {exchange}.")
+        full_ticker: str = f"{ticker}.{exchange}" if exchange else ticker
+        data: yf.Ticker = yf.Ticker(full_ticker, session=session)
+
+        last_price = data.fast_info.last_price
+        if last_price is not None:
+            logger.debug(f"{ticker} price is: {last_price}.")
+            return float(last_price)
+        raise ValueError(f"No price found for {full_ticker}")
+    except Exception as e:
+        raise ValueError(f"Failed to retrieve price for {ticker} at {exchange}: {e}")
