a0328b1a004efaedf888cd3a5ac8e1e4bdedba71
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-05-09 13:52:01 +0800

    chore: move parse_orders() and write_json_to_file() to file_utils.py

--- stock_tracker/main.py
+++ stock_tracker/main.py
@@ -1,8 +1,5 @@
 # Import libraries
-import csv
-import json
 import logging
-from typing import Any
 
 import yfinance as yf
 from stock_tracker.db import Database
@@ -16,31 +13,6 @@ from stock_tracker.yfinance_api import get_yfinance_session, CachedLimiterSessio
 
 
 logger: logging.Logger = logging.getLogger(__name__)
-
-
-def parse_orders(csv_path: str) -> list[dict[str, str]]:
-    orders: list[dict[str, str]] = []
-    with open(file=csv_path, mode="r") as file:
-        reader: csv.DictReader[str] = csv.DictReader(file)
-        for row in reader:
-            # Process each row
-            orders.append(row)
-    return orders
-
-
-def write_json_to_file(data: dict[Any, Any], filename: str) -> None:
-    """Writes Python dictionary data to a JSON file.
-
-    Args:
-      data (dict): The Python dictionary to be written to the file.
-      filename (str): The name of the JSON file to create or overwrite.
-    """
-    try:
-        with open(filename, "w") as json_file:
-            json.dump(data, json_file, indent=4)
-        print(f"Data successfully written to '{filename}'")
-    except IOError as e:
-        print(f"Error writing to file '{filename}': {e}")
 
 
 def main() -> None:
