7ee5a3f01a880cfcf5d1326e3cb4d00094594d71
Cory Gyarmathy <cory.gyarmathy@gmail.com>
2025-05-09 21:44:43 +0800

    fix: fx_rate does not have an ID, so don't generate one

--- stock_tracker/repositories/fx_rate_repository.py
+++ stock_tracker/repositories/fx_rate_repository.py
@@ -1,3 +1,4 @@
+from sqlite3 import Cursor
 from stock_tracker.db import Database
 from stock_tracker.models import FxRate
 
@@ -6,8 +7,8 @@ class FxRateRepository:
     def __init__(self, db: Database):
         self.db = db
 
-    def insert(self, fx_rate: FxRate) -> int:
-        cursor = self.db.execute(
+    def insert(self, fx_rate: FxRate) -> None:
+        cursor: Cursor = self.db.execute(
             """
             INSERT INTO fx_rates (base_currency, target_currency, date, rate)
             VALUES (:base_currency, :target_currency, :date, :rate)
@@ -19,11 +20,6 @@ class FxRateRepository:
                 "rate": fx_rate.rate,
             },
         )
-        fx_rate.id = cursor.lastrowid
-        if fx_rate.id:
-            return fx_rate.id
-        else:
-            raise ValueError(f"Failed to obtain id of fx_rate after inserting into db.")
 
     def get_rate(self, base_currency: str, target_currency: str, date: str) -> FxRate | None:
         row = self.db.query_one(
